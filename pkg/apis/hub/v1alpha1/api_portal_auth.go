30b4c69086d5b3890d6ec0454e02bc8f810f0f50
Harold Ozouf <harold.ozouf@gmail.com>
2025-09-29 14:36:04 +0200

    feat: LDAP support in APIPortalAuth and APIAuth

--- pkg/apis/hub/v1alpha1/api_portal_auth.go
+++ pkg/apis/hub/v1alpha1/api_portal_auth.go
@@ -39,9 +39,15 @@ type APIPortalAuth struct {
 }
 
 // APIPortalAuthSpec configures the authentication for an APIPortal.
+// +kubebuilder:validation:XValidation:message="exactly one of oidc or ldap must be specified",rule="[has(self.oidc), has(self.ldap)].filter(x, x).size() == 1"
 type APIPortalAuthSpec struct {
 	// OIDC configures the OIDC authentication.
-	OIDC OIDCConfig `json:"oidc"`
+	// +optional
+	OIDC *OIDCConfig `json:"oidc,omitempty"`
+
+	// LDAP configures the LDAP authentication.
+	// +optional
+	LDAP *PortalAuthLDAPConfig `json:"ldap,omitempty"`
 }
 
 // OIDCConfig configures OIDC authentication for an APIPortal.
@@ -59,18 +65,17 @@ type OIDCConfig struct {
 	Scopes *[]string `json:"scopes,omitempty"`
 
 	// Claims configures JWT claim mappings for user attributes.
-	Claims ClaimsSpec `json:"claims"`
+	Claims Claims `json:"claims"`
 
-	// SyncedAttributes is a list of additional attributes to sync from the OIDC provider.
-	// Each attribute must correspond to a configured claim field.
+	// SyncedAttributes are the user attributes to synchronize with Hub platform.
 	// +optional
 	// +kubebuilder:validation:MaxItems=6
 	// +kubebuilder:validation:items:Enum=groups;userId;firstname;lastname;email;company
 	SyncedAttributes []string `json:"syncedAttributes,omitempty"`
 }
 
-// ClaimsSpec configures JWT claim mappings for user attributes.
-type ClaimsSpec struct {
+// Claims configures JWT claim mappings for user attributes.
+type Claims struct {
 	// Groups is the JWT claim for user groups. This field is required for authorization.
 	Groups string `json:"groups"`
 
@@ -93,6 +98,101 @@ type ClaimsSpec struct {
 	// Company is the JWT claim for user company.
 	// +optional
 	Company string `json:"company,omitempty"`
+}
+
+// LDAPConnectionConfig holds LDAP connection configuration.
+type LDAPConnectionConfig struct {
+	// URL is the URL of the LDAP server, including the protocol (ldap or ldaps) and the port.
+	// +kubebuilder:validation:XValidation:message="must be a valid LDAP URL",rule="isURL(self) && (self.startsWith('ldap://') || self.startsWith('ldaps://'))"
+	URL string `json:"url"`
+
+	// StartTLS instructs the middleware to issue a StartTLS request when initializing the connection with the LDAP server.
+	// +optional
+	StartTLS bool `json:"startTls,omitempty"`
+
+	// InsecureSkipVerify controls whether the server's certificate chain and host name is verified.
+	// +optional
+	InsecureSkipVerify bool `json:"insecureSkipVerify,omitempty"`
+
+	// CertificateAuthority is a PEM-encoded certificate to use to establish a connection with the LDAP server if the
+	// connection uses TLS but that the certificate was signed by a custom Certificate Authority.
+	// +optional
+	CertificateAuthority string `json:"certificateAuthority,omitempty"`
+
+	// BindDN is the domain name to bind to in order to authenticate to the LDAP server when running in search mode.
+	// If empty, an anonymous bind will be done.
+	// +optional
+	BindDN string `json:"bindDn,omitempty"`
+
+	// BindPasswordSecretName is the name of the Kubernetes Secret containing the password for the bind DN.
+	// The secret must contain a key named 'password'.
+	// +optional
+	// +kubebuilder:validation:MaxLength=253
+	BindPasswordSecretName string `json:"bindPasswordSecretName,omitempty"`
+
+	// BaseDN is the base domain name that should be used for bind and search queries.
+	BaseDN string `json:"baseDn"`
+
+	// Attribute is the LDAP object attribute used to form a bind DN when sending bind queries.
+	// The bind DN is formed as <Attribute>=<Username>,<BaseDN>.
+	// +optional
+	// +kubebuilder:default="cn"
+	Attribute string `json:"attribute,omitempty"`
+
+	// SearchFilter is used to filter LDAP search queries.
+	// Example: (&(objectClass=inetOrgPerson)(gidNumber=500)(uid=%s))
+	// %s can be used as a placeholder for the username.
+	// +optional
+	SearchFilter string `json:"searchFilter,omitempty"`
+}
+
+// PortalAuthLDAPConfig holds LDAP configuration for portal authentication.
+type PortalAuthLDAPConfig struct {
+	LDAPConnectionConfig `json:",inline"`
+
+	// Groups configures group extraction.
+	// +optional
+	Groups *LDAPGroups `json:"groups,omitempty"`
+
+	// Attributes configures LDAP attribute mappings for user attributes.
+	// +optional
+	Attributes *Attributes `json:"attributes"`
+
+	// SyncedAttributes are the user attributes to synchronize with Hub platform.
+	// +optional
+	// +kubebuilder:validation:MaxItems=6
+	// +kubebuilder:validation:items:Enum=groups;userId;firstname;lastname;email;company
+	SyncedAttributes []string `json:"syncedAttributes,omitempty"`
+}
+
+// Attributes configures LDAP attribute mappings for user attributes.
+type Attributes struct {
+	// UserID is the LDAP attribute for user ID mapping.
+	// +optional
+	UserID string `json:"userId,omitempty"`
+
+	// Firstname is the LDAP attribute for user first name.
+	// +optional
+	Firstname string `json:"firstname,omitempty"`
+
+	// Lastname is the LDAP attribute for user last name.
+	// +optional
+	Lastname string `json:"lastname,omitempty"`
+
+	// Email is the LDAP attribute for user email.
+	// +optional
+	Email string `json:"email,omitempty"`
+
+	// Company is the LDAP attribute for user company.
+	// +optional
+	Company string `json:"company,omitempty"`
+}
+
+// LDAPGroups configures LDAP group extraction.
+type LDAPGroups struct {
+	// MemberOfAttribute is the LDAP attribute containing group memberships (e.g., "memberOf").
+	// +kubebuilder:default="memberOf"
+	MemberOfAttribute string `json:"memberOfAttribute,omitempty"`
 }
 
 // APIPortalAuthStatus is the status of an APIPortalAuth.
--- pkg/validation/v1alpha1/api_portal_auth_test.go
+++ pkg/validation/v1alpha1/api_portal_auth_test.go
@@ -46,7 +46,7 @@ spec:
 			wantErrs: field.ErrorList{{Type: field.ErrorTypeRequired, Field: "metadata.namespace", BadValue: ""}},
 		},
 		{
-			desc: "valid: minimal configuration",
+			desc: "valid: minimal OIDC configuration",
 			manifest: []byte(`
 apiVersion: hub.traefik.io/v1alpha1
 kind: APIPortalAuth
@@ -59,6 +59,19 @@ spec:
     secretName: "oidc-secret"
     claims:
       groups: "groups"`),
+		},
+		{
+			desc: "valid: minimal LDAP configuration",
+			manifest: []byte(`
+apiVersion: hub.traefik.io/v1alpha1
+kind: APIPortalAuth
+metadata:
+  name: my-ldap-auth
+  namespace: default
+spec:
+  ldap:
+    url: "ldap://ldap.example.com:389"
+    baseDn: "dc=example,dc=com"`),
 		},
 		{
 			desc: "valid: full configuration",
@@ -244,6 +257,168 @@ spec:
     syncedAttributes:
       - "unknownField"`),
 			wantErrs: field.ErrorList{{Type: field.ErrorTypeNotSupported, Field: "spec.oidc.syncedAttributes[0]", BadValue: "unknownField", Detail: "supported values: \"groups\", \"userId\", \"firstname\", \"lastname\", \"email\", \"company\""}},
+		},
+		{
+			desc: "valid: full LDAP configuration",
+			manifest: []byte(`
+apiVersion: hub.traefik.io/v1alpha1
+kind: APIPortalAuth
+metadata:
+  name: my-ldap-auth
+  namespace: default
+spec:
+  ldap:
+    url: "ldaps://ldap.example.com:636"
+    startTls: true
+    insecureSkipVerify: false
+    certificateAuthority: |
+      -----BEGIN CERTIFICATE-----
+      MIIBCzCBsqADAgECAhBaooOsws+BLdvtfqQ1ggx5MAoGCCqGSM49BAMCMBIxEDAO
+      BgNVBAoTB0V4YW1wbGUwHhcNMjQwMTAxMDAwMDAwWhcNMjUwMTAxMDAwMDAwWjAS
+      MRAwDgYDVQQKEwdFeGFtcGxlMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEKNho
+      zEli5D+VsLgKJcgT0rp+MnYGJ4PjN8qgXfQx1F5JhtVBVnH8qWmza2XwJvVZAgUg
+      WijH8vDvBJU8su1w16MdMBswDgYDVR0PAQH/BAQDAgWgMAkGA1UdEwQCMAAwCgYI
+      KoZIzj0EAwIDSAAwRQIgcu4/UZKPaUPCAB2jjqKbW8XqBp8fv1F8D5FO5hL1DqwC
+      IQDB3g0Lhx4QbM3Kw6bk0gvvVVLkb/TXe2Nvl4dH8gOCEw==
+      -----END CERTIFICATE-----
+    bindDn: "cn=admin,dc=example,dc=com"
+    bindPasswordSecretName: "ldap-bind-password"
+    baseDn: "dc=example,dc=com"
+    attribute: "uid"
+    searchFilter: "(&(objectClass=inetOrgPerson)(uid=%s))"
+    groups:
+      memberOfAttribute: "memberOf"
+    attributes:
+      userId: "uid"
+      firstname: "givenName"
+      email: "mail"
+    syncedAttributes:
+      - "userId"
+      - "firstname"
+      - "email"`),
+		},
+		{
+			desc: "invalid: both OIDC and LDAP configured",
+			manifest: []byte(`
+apiVersion: hub.traefik.io/v1alpha1
+kind: APIPortalAuth
+metadata:
+  name: my-auth
+  namespace: default
+spec:
+  oidc:
+    issuerUrl: "https://auth.example.com"
+    secretName: "oidc-secret"
+    claims:
+      groups: "groups"
+  ldap:
+    url: "ldap://ldap.example.com:389"
+    baseDn: "dc=example,dc=com"`),
+			wantErrs: field.ErrorList{{Type: field.ErrorTypeInvalid, Field: "spec", BadValue: "object", Detail: "exactly one of oidc or ldap must be specified"}},
+		},
+		{
+			desc: "invalid: neither OIDC nor LDAP configured",
+			manifest: []byte(`
+apiVersion: hub.traefik.io/v1alpha1
+kind: APIPortalAuth
+metadata:
+  name: my-auth
+  namespace: default
+spec: {}`),
+			wantErrs: field.ErrorList{{Type: field.ErrorTypeInvalid, Field: "spec", BadValue: "object", Detail: "exactly one of oidc or ldap must be specified"}},
+		},
+		{
+			desc: "invalid: LDAP missing required URL",
+			manifest: []byte(`
+apiVersion: hub.traefik.io/v1alpha1
+kind: APIPortalAuth
+metadata:
+  name: my-ldap-auth
+  namespace: default
+spec:
+  ldap:
+    baseDn: "dc=example,dc=com"`),
+			wantErrs: field.ErrorList{{Type: field.ErrorTypeRequired, Field: "spec.ldap.url", BadValue: ""}},
+		},
+		{
+			desc: "invalid: LDAP missing required baseDn",
+			manifest: []byte(`
+apiVersion: hub.traefik.io/v1alpha1
+kind: APIPortalAuth
+metadata:
+  name: my-ldap-auth
+  namespace: default
+spec:
+  ldap:
+    url: "ldap://ldap.example.com:389"`),
+			wantErrs: field.ErrorList{{Type: field.ErrorTypeRequired, Field: "spec.ldap.baseDn", BadValue: ""}},
+		},
+		{
+			desc: "invalid: LDAP invalid URL format",
+			manifest: []byte(`
+apiVersion: hub.traefik.io/v1alpha1
+kind: APIPortalAuth
+metadata:
+  name: my-ldap-auth
+  namespace: default
+spec:
+  ldap:
+    url: "https://ldap.example.com"
+    baseDn: "dc=example,dc=com"`),
+			wantErrs: field.ErrorList{{Type: field.ErrorTypeInvalid, Field: "spec.ldap.url", BadValue: "string", Detail: "must be a valid LDAP URL"}},
+		},
+		{
+			desc: "invalid: LDAP bindPasswordSecretName too long",
+			manifest: []byte(`
+apiVersion: hub.traefik.io/v1alpha1
+kind: APIPortalAuth
+metadata:
+  name: my-ldap-auth
+  namespace: default
+spec:
+  ldap:
+    url: "ldap://ldap.example.com:389"
+    baseDn: "dc=example,dc=com"
+    bindPasswordSecretName: "` + tooLongSecretName + `"`),
+			wantErrs: field.ErrorList{{Type: field.ErrorTypeTooLong, Field: "spec.ldap.bindPasswordSecretName", BadValue: "<value omitted>", Detail: "may not be more than 253 bytes"}},
+		},
+		{
+			desc: "valid: LDAP with syncedAttributes",
+			manifest: []byte(`
+apiVersion: hub.traefik.io/v1alpha1
+kind: APIPortalAuth
+metadata:
+  name: my-ldap-auth
+  namespace: default
+spec:
+  ldap:
+    url: "ldap://ldap.example.com:389"
+    baseDn: "dc=example,dc=com"
+    groups:
+      memberOfAttribute: "memberOf"
+    attributes:
+      userId: "uid"
+      email: "mail"
+    syncedAttributes:
+      - "groups"
+      - "userId"
+      - "email"`),
+		},
+		{
+			desc: "invalid: LDAP syncedAttributes contains unknown field",
+			manifest: []byte(`
+apiVersion: hub.traefik.io/v1alpha1
+kind: APIPortalAuth
+metadata:
+  name: my-ldap-auth
+  namespace: default
+spec:
+  ldap:
+    url: "ldap://ldap.example.com:389"
+    baseDn: "dc=example,dc=com"
+    syncedAttributes:
+      - "unknownField"`),
+			wantErrs: field.ErrorList{{Type: field.ErrorTypeNotSupported, Field: "spec.ldap.syncedAttributes[0]", BadValue: "unknownField", Detail: "supported values: \"groups\", \"userId\", \"firstname\", \"lastname\", \"email\", \"company\""}},
 		},
 	}
 
